4f2d5d26b196acd042e1a707dd212f5811af2fae
hemanth yerra <hemanthyerra9@gmail.com>
2025-06-29 00:54:35 +0530

    applied react routing to the application starting with home page and starred page. react router dom helps in navigating between pages but it is actually a address based rendering

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import {BrowserRouter,Route,Routes} from 'react-router-dom';
+import Home from './pages/Home';
+import Starred from './pages/Starred';
+function App() {
+  return (
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="/starred" element={<Starred/>} />
+          <Route path="*" element={<div>Not found</div>} />
+        </Routes>
+      </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/pages/Starred.jsx
@@ -0,0 +1,9 @@
+function Starred() {
+  return (
+    <div>
+      Starred Page
+      <p>This page will display the starred items.</p>
+    </div>
+  );
+}
+export default Starred;
