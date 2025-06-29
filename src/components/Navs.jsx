1b825f60701935edd5df97b6b402c925c643f46f
hemanth yerra <hemanthyerra9@gmail.com>
2025-06-29 11:19:39 +0530

    integrated tvmaze api to fetch the search results into console

--- src/components/Navs.jsx
+++ src/components/Navs.jsx
@@ -1,26 +1,24 @@
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
-const LINKS = [{
-  text:"Home",
-  to:"/"
-},
-{
-  text:"Starred",
-  to:"/starred"
-}
+const LINKS = [
+  { text: "Home", to: "/" },
+  { text: "Starred", to: "/starred" }
 ];
-function Navs(){
+
+function Navs() {
   return (
     <div>
       <nav>
         <ul>
-          {LINKS.map((item)=>(
-            <li key= "item.to">
-              <Link to={item.to}>{item.text}</Link></li>
+          {LINKS.map(item => (
+            <li key={item.to}>
+              <Link to={item.to}>{item.text}</Link>
+            </li>
           ))}
         </ul>
       </nav>
     </div>
   );
 }
+
 export default Navs;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,27 @@
-
+import { useState } from "react";
 function Home() {
+  const [Searchstr,setSearchstr] = useState("");
+
+  const oninputValueChange = (ev) => {
+    setSearchstr(ev.target.value);
+  }
+
+  const onSearch= async (ev) => {
+    ev.preventDefault();
+
+    const response = await fetch(`https://api.tvmaze.com/search/shows?q=${Searchstr}`);
+    const body = await response.json();
+    console.log(body);
+  }
+  
   return (
     <div className="home">
       <h1>Welcome to the Home Page</h1>
-      <p>This is the main page of our application.</p>
+      <div>{Searchstr}</div>
+      <form onSubmit={onSearch}>
+        <input type="text" value={Searchstr} onChange={oninputValueChange}/>
+        <button type="submit">Search</button>
+      </form>
       
     </div>
   );
